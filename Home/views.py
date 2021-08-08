f9a435f58451114f0f2f098b93d1cfef13701f4d
Ravi Muduthanapalli <ravimuduthana@gmail.com>
2021-08-08 17:39:27 +0530

    changes

--- Home/views.py
+++ Home/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def index(request):
-        name = {'sample_file': "gettooltiptext.html",'sample_video': "https://www.youtube.com/embed/kbizBdP-Cqw" }
+        name = {'sample_file': "gettooltiptext.html",'sample_video': "https://www.youtube.com/embed/L0I5TSDrR4A" }
         return render(request,"index.html",name)
 def TestPage(request):
     return render(request,"TestPage.html")
@@ -12,7 +12,7 @@ def TestPage(request):
 
 def gettooltiptext(request):
     #return HttpResponse("Updated code on PythonAnywhere")
-    name = {'sample_file': "gettooltiptext.html",'sample_video': "https://www.youtube.com/embed/kbizBdP-Cqw" }
+    name = {'sample_file': "gettooltiptext.html",'sample_video': "https://www.youtube.com/embed/L0I5TSDrR4A" }
     return render(request,"index.html",name)
 def Radioboxpage(request):
         return render(request,"Radioboxpage.html")
@@ -33,5 +33,5 @@ def Textboxpage(request):
 def Checkboxpage(request):
         return render(request,"Checkboxpage.html")
 def webdrivermanager(request):
-        name = {'sample_file': "webdrivermanager.html",'sample_video': "https://www.youtube.com/embed/kbizBdP-Cqw" }
+        name = {'sample_file': "webdrivermanager.html",'sample_video': "https://www.youtube.com/embed/f4qSxXWmRIA" }
         return render(request,"index.html",name)
